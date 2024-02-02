7ea40e3a15b8736ff62de8984d726ec87993f462
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-02 15:35:31 -0600

    basic ability to tell if a room exists in the rooms obj

---
+++ src/Test/Test.js
@@ -0,0 +1,34 @@
+const rooms = {
+    '0.0': { description: "This is red room.", isOpen: 1 },
+    '0.1': { description: "This is green room.", isOpen: 1 },
+    '0.2': { description: "This is blue room.", isOpen: 1 },
+    '1.0': { description: "This is horse room.", isOpen: 1 },
+    '1.1': { description: "This is cow room.", isOpen: 1 },
+    '1.2': { description: "This is cat room.", isOpen: 1 },
+    '2.0': { description: "This is French room.", isOpen: 1 },
+    '2.1': { description: "This is German room.", isOpen: 1 },
+    '2.2': { description: "This is Swedish room.", isOpen: 1 },
+}
+
+let history = []
+
+let userLocation = '0.0'
+
+const move = (dir) => {
+    let coords = userLocation.split('.')
+    console.log(coords);
+    switch (dir) {
+        case 'up':
+            coords.splice(1, 1, coords[1] - 1)
+            
+            console.log(coords.join('.'));
+            if (Object.hasOwn(rooms, coords.join('.'))) {
+                console.log('Has Property');
+            } else {
+                console.log('Doesn\'t have property');
+            }
+            break
+    }
+}
+
+move('up')
